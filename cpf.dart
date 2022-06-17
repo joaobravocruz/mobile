a3a072a261acc5cd9520e82855294fa3224cac0f
João Cruz <joaocruz@vetorial.net>
2022-06-17 18:00:48 -0300

    enxugando codigo

--- cpf.dart
+++ cpf.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 main() {
   stdout.write('Cpf: ');
   String? cpfInicial = stdin.readLineSync().toString();
+
   cpfInicial.split('');
 
   int controle = 10;
@@ -15,9 +16,7 @@ main() {
   int soma = cpfCalculado.reduce((value, element) => value + element);
   int modulo = soma % 11;
   int resto = 11 - modulo;
-  if (resto > 9) {
-    resto = 0;
-  }
+  int digitoVerificador = (resto > 9) ? 0 : resto;
 
   int controle2 = 11;
   List<int> cpfCalculado2 = [];
@@ -30,14 +29,9 @@ main() {
   int soma2 = cpfCalculado2.reduce((value, element) => value + element);
   int modulo2 = soma2 % 11;
   int resto2 = 11 - modulo2;
-  if (resto2 > 9) {
-    resto2 = 0;
-  }
+  int digitoVerificador2 = (resto2 > 9) ? 0 : resto2;
 
-  if (int.parse(cpfInicial[10]) == resto2 &&
-      int.parse(cpfInicial[9]) == resto) {
-    print('OK');
-  } else {
-    print('ERRO');
-  }
+  bool validado = ((int.parse(cpfInicial[10]) == digitoVerificador2 &&
+      int.parse(cpfInicial[9]) == digitoVerificador));
+  print(validado);
 }
