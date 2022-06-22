e103babea6b754d1e1a0e5428f8a0e889601e2e0
João Cruz <joaocruz@vetorial.net>
2022-06-22 14:25:49 -0300

    Retonar se CPF é válido ou não

--- cpf.dart
+++ cpf.dart
@@ -1,36 +1,31 @@
 import 'dart:io';
 
-main() {
-  stdout.write('Cpf: ');
-  String? cpfInicial = stdin.readLineSync().toString();
-
+ValidarCpf(cpfInicial) {
   cpfInicial.split('');
 
-  int controle = 10;
-  List<int> cpfCalculado = [];
-  for (int i = 0; i <= 8; i++) {
-    int calculoCpf = (int.parse(cpfInicial[i]) * controle);
-    cpfCalculado.add(calculoCpf);
-    controle--;
-  }
-  int soma = cpfCalculado.reduce((value, element) => value + element);
-  int modulo = soma % 11;
-  int resto = 11 - modulo;
-  int digitoVerificador = (resto > 9) ? 0 : resto;
+  int soma = 0;
 
-  int controle2 = 11;
-  List<int> cpfCalculado2 = [];
-  for (int i = 0; i <= 9; i++) {
-    int calculoCpf2 = (int.parse(cpfInicial[i]) * controle2);
-    cpfCalculado2.add(calculoCpf2);
-    controle2--;
+  for (int i = 1; i <= 9; i++) {
+    soma = soma + int.parse(cpfInicial.substring(i - 1, i)) * (11 - i);
   }
-  int soma2 = cpfCalculado2.reduce((value, element) => value + element);
-  int modulo2 = soma2 % 11;
-  int resto2 = 11 - modulo2;
+  int resto = (soma * 10) % 11;
+  int digitoVerificador1 = (resto > 9) ? 0 : resto;
+
+  soma = 0;
+
+  for (int i = 1; i <= 10; i++) {
+    soma = soma + int.parse(cpfInicial.substring(i - 1, i)) * (12 - i);
+  }
+  int resto2 = (soma * 10) % 11;
   int digitoVerificador2 = (resto2 > 9) ? 0 : resto2;
 
   bool validado = ((int.parse(cpfInicial[10]) == digitoVerificador2 &&
-      int.parse(cpfInicial[9]) == digitoVerificador));
+      int.parse(cpfInicial[9]) == digitoVerificador1));
   print(validado);
+}
+
+main() {
+  stdout.write('Cpf: ');
+  String? cpfInicial = stdin.readLineSync().toString();
+  ValidarCpf(cpfInicial);
 }
